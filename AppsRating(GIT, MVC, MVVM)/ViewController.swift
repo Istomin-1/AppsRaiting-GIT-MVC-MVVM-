ce0d7ff1b9294b740764479e786ba8d2d3544cd2
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-03-28 14:44:07 +0500

    Initial Commit

---
+++ AppsRating(GIT, MVC, MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppsRating(GIT, MVC, MVVM)
+//
+//  Created by Mikhail on 28.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
